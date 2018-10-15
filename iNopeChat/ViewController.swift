9212f300c8fc80cfc1d4515af6ccd4daa7298202
Jordan Fallon <thefetts@gmail.com>
2018-10-15 10:55:40 -0600

    Add animated title

--- iNopeChat/ViewController.swift
+++ iNopeChat/ViewController.swift
@@ -3,11 +3,15 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var lookUponMyWorks: UITextField!
     @IBOutlet var andDespair: UILabel!
+    @IBOutlet var heisenberg: UILabel!
     var ozymandias: Timer!
+    var sayMyName: Timer!
+    var count = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         ozymandias = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(thisTooShallPass), userInfo: nil, repeats: true)
+        sayMyName = Timer.scheduledTimer(timeInterval: 0.25, target: self, selector: #selector(youreGodDamnRight), userInfo: nil, repeats: true)
     }
 
     @objc
@@ -22,5 +26,23 @@ class ViewController: UIViewController {
                 }
             }
         }
+    }
+
+    @objc
+    func youreGodDamnRight() {
+        let fullName = "nopechat"
+        count += 1
+        if(count > fullName.count + 5) {
+            count = 0
+        }
+        heisenberg.text = fullName.upTo(i: count)
+    }
+}
+
+extension String {
+    func upTo(i: Int) -> String {
+        let offset = i > endIndex.encodedOffset ? self.count : i
+        let end = self.index(startIndex, offsetBy: offset)
+        return String(self[startIndex..<end])
     }
 }
